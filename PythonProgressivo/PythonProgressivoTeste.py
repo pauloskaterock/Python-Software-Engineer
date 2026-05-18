cc5a5c2c6b78aabc575d71379f7940757c39bf5f
paulinho <pauloengenharia5@yahoo.com>
2026-05-18 15:17:36 -0300

    operacoes matematicas

--- PythonProgressivo/PythonProgressivoTeste.py
+++ PythonProgressivo/PythonProgressivoTeste.py
@@ -1,1 +1,93 @@
-print ('Ola python')
+# # print ('Ola python')
+
+
+# # Crie um programa que exiba na tela seu nome completo, sua cidade, estado e data de nascimento.
+
+# print('Paulinho Robotics Python Automation Engineer')
+# nome = 'Paulinho'
+# idade = 38
+# cidade = 'dirtyMaua'
+# estado = 'sp'
+# nascimento = 1988
+
+# print('Seu nome é:', nome)
+# print('Sua idade é:', idade)
+# print('Sua cidade é:', cidade)
+# print('Seu estado é:', estado)
+# print('Sua data de nascimento é:', nascimento)
+
+
+# #-------------------------------------------------------------------------
+
+# # Crie um programa que exiba na tela o texto 'A melhor banda do mundo é [nome da banda] e a melhor música é [nome da música]'.
+
+# band = 'zumbis do espaço'
+# musica = 'chamado da estrada'
+
+# print('A melhor banda do mundo é' , band,  ' e a melhor música é ', musica)
+
+
+# #---------------------------------------------------------------------------
+
+# salario=0.0
+# print('Antes de ser programdor eu ganhava R$',salario,'por mes')
+
+# salario=5000.0
+# print('Agora virei programador Python e ganho R$',salario)
+
+# #-------------------------------------------------------------------------
+
+# # Exercício: O seguinte código imprime todas as palavras reservadas (keywords) do Python, rode ele e escreva nos comentários o resultado:
+
+# import keyword
+# print(keyword.kwlist)
+
+# #---------------------------------------------------------------------------
+
+# idade=input('Qual sua idade? ')
+# nome=input('Qual seu nome? ')
+
+# print('Olá, seu nome é ', nome, ' e tem ', idade, ' anos de idade! Seja bem vindo ao Curso Python Progressivo')
+# #--------------------------------------------------------------------
+# string=input("Digite uma string: ")
+# print( type(string) )
+
+# inteiro=input("Digite um inteiro: ")
+# print( type(inteiro) )
+
+# decimal=input("Digite um float: ")
+# print( type(decimal) )
+
+
+
+# var1 = '2112'
+# print(type())
+
+# var2 = int(var1)
+# print( type(var2) )
+
+
+
+
+# var1=input("Digite um decimal: ")
+# print( type(var1) )
+
+# var2=float(var1)
+# print( type(var2) )
+
+# var3=2112
+# var4=float(var3)
+
+# print( type(var4) )
+
+# print(var4)
+
+
+# var1 = int( input("Digite um inteiro: ") )
+#------------------------------------------------------------------------
+var1 = int( input("Digite um inteiro: ") )
+var2 = int( input("Digite outro inteiro: ") )
+
+produto = var1 ** var2
+
+print(produto)
